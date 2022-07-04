bdcc087465df957ba9ca18e229e0e3f26082332e
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-07-04 13:08:08 +0530

    fixed linking bugs

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,27 +1,16 @@
 import React from 'react'
 import {BsTwitter, BsInstagram, BsLinkedin} from 'react-icons/bs'
-function social(item){
-  if(item=="twitter"){
-    window.open("https://twitter.com/", "_blank")
-  }
-  else if(item=="linkedIn"){
-    window.open("https://twitter.com/", "_blank")
-  }
-  else if(item=="insta"){
-    window.open("https://twitter.com/", "_blank")
-  }
-}
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter onClick={social("twitter")}/>
+            <BsTwitter/>
         </div>
         <div>
-            <BsLinkedin onClick={social("linkedIn")}/>
+            <BsLinkedin />
         </div>
         <div>
-            <BsInstagram onClick={social("insta")}/>
+            <BsInstagram />
         </div>
     </div>
   )
